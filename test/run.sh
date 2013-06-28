7f12edde13a8cbe436f458bbd5b769bb12b2f89d
ka <ka.kaosf@gmail.com>
2013-06-28 15:01:04 +0900

    show test results

--- test/run.sh
+++ test/run.sh
@@ -8,19 +8,25 @@ cd test
   set -e
 
   # Example 1
+  echo 'Example 1'
   cp a.txt a.txt.bk
-  $TOOL a.txt abc def
+  $TOOL a.txt abc def > /dev/null 2>&1
   diff a.txt 1-expected.txt
+  echo 'OK'
 
   # Example 5
+  echo 'Example 5'
   cp a.txt.bk a.txt
-  $TOOL a.txt abc def 2
+  $TOOL a.txt abc def 2 > /dev/null 2>&1
   diff a.txt 5-expected.txt
+  echo 'OK'
 
   # Example 6
+  echo 'Example 6'
   cp a.txt.bk a.txt
-  $TOOL a.txt cde def -2
+  $TOOL a.txt cde def -2 > /dev/null 2>&1
   diff a.txt 6-expected.txt
+  echo 'OK'
 )
 # catch
 [ $? -eq 0 ] || (
