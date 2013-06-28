f7fe7bb19af5e157533f6952886ef440d9086bd6
ka <ka.kaosf@gmail.com>
2013-06-28 18:16:00 +0900

    use shunit2 and fix README and test/run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,44 +1,34 @@
 #! /bin/sh
 
+cd `dirname $0`
 TOOL=../semi-automatic-text-editor
-cd test
 
-# try
-(
-  set -e
-
-  # Example 1
-  echo 'Example 1'
+testExample1()
+{
   cp a.txt a.txt.bk
   $TOOL a.txt abc def > /dev/null 2>&1
   diff a.txt 1-expected.txt
-  echo 'OK'
+  assertEquals 0 $?
+  mv a.txt.bk a.txt
+}
 
-  # Example 5
-  echo 'Example 5'
-  cp a.txt.bk a.txt
+testExample5()
+{
+  cp a.txt a.txt.bk
   $TOOL a.txt abc def 2 > /dev/null 2>&1
   diff a.txt 5-expected.txt
-  echo 'OK'
+  assertEquals 0 $?
+  mv a.txt.bk a.txt
+}
 
-  # Example 6
-  echo 'Example 6'
-  cp a.txt.bk a.txt
+testExample6()
+{
+  cp a.txt a.txt.bk
   $TOOL a.txt cde def -2 > /dev/null 2>&1
   diff a.txt 6-expected.txt
-  echo 'OK'
-)
-# catch
-[ $? -eq 0 ] || (
-  RET=$?
-  echo 'error!' 2>&1
-  exit $RET
-)
-# finally
-{
-  RET=$?
-
+  assertEquals 0 $?
   mv a.txt.bk a.txt
-
-  exit $RET
 }
+
+# load shunit2
+. ./shunit2
